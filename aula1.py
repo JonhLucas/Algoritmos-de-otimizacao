57597e2a34e65a5e9387c7a037b630bb22e4618a
JonhLucas <38635930+JonhLucas@users.noreply.github.com>
2019-12-05 17:25:41 -0300

    aula1
    funcao de custo

--- aula1.py
+++ aula1.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import time
+import random
+import math
 
 pessoas = [('Amanda', 'CWB'), ('Pedro', 'GIG'), ('Marcos', 'POA'),
            ('Priscila', 'FLN'), ('Jessica', 'CNF'), ('Paulo','GYN')]
@@ -34,3 +36,50 @@ agenda = [1,4, 3,2, 7,3, 6,3 , 2,4, 5,3]
 imprimir_agenda(agenda)
 get_minutos('6:13')
 get_minutos('15:30')
+
+def funcao_custo(solucao):
+    preco_total = 0
+    ultima_chegada = 0
+    primeira_partida = 1439
+    
+    id_voo = -1
+    for i in range(len(solucao)//2):
+        destino = 'GRU'
+        origem = pessoas[i][1]
+        id_voo += 1
+        ida = voos[(origem, destino)][solucao[id_voo]]
+        id_voo += 1
+        volta = voos[(destino, origem)][solucao[id_voo]]
+        
+        preco_total += ida[2]
+        preco_total += volta[2]
+        
+        if ultima_chegada < get_minutos(ida[1]):
+            ultima_chegada = get_minutos(ida[1])
+            
+        if primeira_partida > get_minutos(volta[0]):
+            primeira_partida = get_minutos(volta[0])
+    print(preco_total, ultima_chegada, primeira_partida)
+    id_voo = -1
+    total_espera = 0
+    for i in range(len(solucao)//2):
+        origem = pessoas[i][1]
+        id_voo += 1
+        ida = voos[(origem, destino)][solucao[id_voo]]
+        id_voo += 1
+        volta = voos[(destino, origem)][solucao[id_voo]]
+        total_espera += ultima_chegada - get_minutos(ida[1])
+        total_espera += get_minutos(volta[0]) - primeira_partida
+        
+    if ultima_chegada > primeira_partida:
+        preco_total += 50
+        
+    return preco_total + total_espera
+    
+funcao_custo(agenda)
+    
+    
+    
+    
+    
+
